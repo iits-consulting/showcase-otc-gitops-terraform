070ae0d32b9eb4ab71dece141e3c63ecb1f1b2d4
Victor Getz <sd>
2022-01-27 14:32:01 +0100

    Prepare for talk

--- context/dev/providers.tf
+++
@@ -1,9 +0,0 @@
-provider "opentelekomcloud" {
-  auth_url    = "https://iam.eu-de.otc.t-systems.com/v3"
-}
-
-provider "opentelekomcloud" {
-  auth_url    = "https://iam.eu-de.otc.t-systems.com/v3"
-  tenant_name = var.region
-  alias = "top_level_project"
-}
